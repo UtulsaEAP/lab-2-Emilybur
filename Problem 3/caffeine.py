0a16409a25ef216dcf6c6ddfbe4a4c91ab3537ac
Emily Burke <erb9078@utulsa.edu>
2024-02-09 16:14:50 -0600

    finished problem 3

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,6 +1,9 @@
 
 def caffeine():
     caffeine_mg = float(input())
+    print(f'After 6 hours: {caffeine_mg/2:.2f} mg')
+    print(f'After 12 hours: {caffeine_mg/4:.2f} mg')
+    print(f'After 24 hours: {caffeine_mg/16:.2f} mg')
     ''' Type your code here. '''
     
 if __name__ == "__main__":
